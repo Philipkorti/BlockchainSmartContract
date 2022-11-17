25caa514e8325695d9567b2ea395a42d91a4fc8d
Philip <philip.korthauer@gmx.at>
2022-11-17 22:38:51 +0100

    Create Geter Seter Method in the Classes Transaction and BlockHeader

--- BlockChain/src/BlockHeader.java
+++ BlockChain/src/BlockHeader.java
@@ -1,3 +1,58 @@
-public class BlockHeader {
+import java.util.Date;
 
+public class BlockHeader {
+    private int versionsNumber;
+    private long tinmeStamp;
+    private byte[]  previousHash;
+    private byte[] transaktionsListeHash;
+    private int nonce;
+
+    public BlockHeader(long timestamp, byte[] previousHash, byte[] transaktionsListeHash){
+        this.tinmeStamp = timestamp;
+        this.versionsNumber = 1;
+        this.previousHash = previousHash;
+        this.transaktionsListeHash = transaktionsListeHash;
+    }
+    public int GetVersionNumber() {
+        return this.versionsNumber;
+    }
+
+    public void SetVersionNumber(int versionNumber) {
+        this.versionsNumber = versionNumber;
+    }
+
+    public long GetTimeStamp() {
+        return this.tinmeStamp;
+    }
+
+    public void SetTimeStamp(long timeStamp) {
+        this.tinmeStamp = timeStamp;
+    }
+
+    public byte[] GetBlockChainID() {
+        return this.previousHash;
+    }
+
+    public void SetBlockChainID(byte[] previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public byte[] GetTransaktionsListe() {
+        return this.transaktionsListeHash;
+    }
+    public void SetTransaktionsListe(byte[] transaktionsListe){
+        this .transaktionsListeHash = transaktionsListe;
+    }
+    public int GetNonce(){
+        return this.nonce;
+    }
+    public void SetNonce(int nonce){
+        this.nonce = nonce;
+    }
+    public void IncremantNonce() throws ArithmeticException{
+        if (this.nonce == Integer.MAX_VALUE){
+            throw new ArithmeticException("The integer is to heigh!");
+        }
+        this.nonce++;
+    }
 }
--- BlockChain/src/Transaction.java
+++ BlockChain/src/Transaction.java
@@ -3,22 +3,56 @@ import java.io.Serializable;
 import java.nio.charset.Charset;
 
 public class Transaction implements Serializable {
-private byte[] sender;
-private byte[] receiver;
-private double amount;
-private int nonce;
-private double transactionFeeBasePrice;
-private double transectionFeeLimit;
-private transient byte[] txId;
+    private transient byte[] txId;
 
-public Transaction(String sender, String receiver, double amount, int nonce, double transectionFeeBasePrice, double transectionFeeLimit){
-    this.sender = sender.getBytes(Charset.forName("utf8"));
-    this.receiver = receiver.getBytes(Charset.forName("utf8"));
-    this.amount = amount;
-    this.nonce = nonce;
-    this.transactionFeeBasePrice = transectionFeeBasePrice;
-    this.transectionFeeLimit = transectionFeeLimit;
+    private byte[] sender;
+    private byte[] receiver;
+    private double amount;
+    private int nonce;
+    private double transactionFeeBasePrice;
+    private double transectionFeeLimit;
 
-    this.txId = SHA3Helper.hash256(this);
-}
+    private transient long receivedTimeStamp;
+    private transient double transactionFee;
+
+    private transient int sizeInByte;
+
+    private transient byte[] blockId;
+
+    public Transaction(String sender, String receiver, double amount, int nonce, double transectionFeeBasePrice, double transectionFeeLimit) {
+        this.sender = sender.getBytes(Charset.forName("utf8"));
+        this.receiver = receiver.getBytes(Charset.forName("utf8"));
+        this.amount = amount;
+        this.nonce = nonce;
+        this.transactionFeeBasePrice = transectionFeeBasePrice;
+        this.transectionFeeLimit = transectionFeeLimit;
+
+        this.blockId = SHA3Helper.hash256(this);
+    }
+
+    public byte[] GetSender(){
+        return this.sender;
+    }
+    public void SetSender(byte[] sender){
+        this.sender = sender;
+    }
+    public byte[] GetReceiver(){
+        return this.receiver;
+    }
+    public void SetReceiver(byte[] receiver){
+        this.receiver = receiver;
+    }
+    public double GetAmout(){
+        return amount;
+    }
+    public void SetAmout(double amount){
+        this.amount = amount;
+    }
+    public double GetTransactionFeeBasePrice(){
+        return transactionFeeBasePrice;
+    }
+    public void SetTransactionFeeBasePrice(double transactionFeeBasePrice){
+        this.transactionFeeBasePrice = transactionFeeBasePrice;
+    }
+
 }
